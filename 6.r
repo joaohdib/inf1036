c4ce5364183ed125cf9da7d80e2140e9c09caa93
João Henrique Dib <112108662+joaohdib@users.noreply.github.com>
2025-08-28 09:41:58 -0300

    Enhance sampling function with multiple outcomes

--- 6.r
+++ 6.r
@@ -1,24 +1,33 @@
 #tentar implementar a função sample
 # sample(c("n","c1","c2","c3","c4"), 1, prob = c(0.8,0.04,0.04,0.06,0.06)) FALTA ACABAR
 
-vetor <- c("cara","coroa")
+vetor <- c("cara","coroa", "sla")
 qtd <- 1
-prob <- c(0.5,0.5)
-nsamples <- 0
+prob <- c(0.3,0.3,0.4)
+nsamples <- 100000
 
-for (i in 1:qtd) {
-  n <- runif(1)
-  print(n)
-  for (i in 1:vetor) {
-    if (n <= prob[1]) {
-      result <- vetor[1]
+for (j in 1:nsamples) {
+    for (i in 1:qtd) {
+    n <- runif(1)
+    print(n)
+    for (i in 1:length(vetor)) {
+      if (n >= sum(prob[1:i-1])) {
+        result <- vetor[i]
+      }
     }
-    else if (n > prob[2]) {
-      result <- vetor[2]
+    if (result == "cara") {
+      qtdC <- qtdC + 1
+    }
+    else if (result == "coroa") {
+      qtdCo <- qtdCo + 1
+    }
+    
+    else {
+      qtdSla <- qtdSla + 1
+    }
+    print(result)
     }
   }
-  print(result)
-}
 
 
 
