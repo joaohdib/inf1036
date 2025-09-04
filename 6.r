0bfb11e27b2f8fe76829e237aaab551bff9f68ff
João Henrique Dib <112108662+joaohdib@users.noreply.github.com>
2025-09-04 10:04:29 -0300

    Refactor sampling function and reduce sample size
    Reduced the number of samples from 100000 to 100 and refactored the sampling function to improve efficiency.

--- 6.r
+++ 6.r
@@ -4,17 +4,33 @@
 vetor <- c("cara","coroa", "sla")
 qtd <- 1
 prob <- c(0.3,0.3,0.4)
-nsamples <- 100000
+nsamples <- 100
+
+
+
+
+sample2 <- function(qtd, prob, vetor) {
+    result <- character(0)
+    somaAnterior <- 0 
+    for (i in 1:qtd) {
+      n <- runif(1)
+      for (i in 1:length(vetor)) {
+        soma <- somaAnterior + prob[i]
+        if (n >= somaAnterior && n <= soma) {
+          result <- c(result,vetor[i])
+          break
+        } 
+      somaAnterior <- soma
+      }
+  }
+    
+    if (qtd == 1) {
+      return (result[1])
+    } else {
+      return (result)
+    }
+}
 
 for (j in 1:nsamples) {
-    for (i in 1:qtd) {
-    n <- runif(1)
-    print(n)
-    for (i in 1:length(vetor)) {
-      if (n >= sum(prob[1:i-1])) {
-        result <- vetor[i]
-      }
-    }
-    print(result)
-    }
-  }
+  print(sample2(qtd,prob,vetor))
+}
